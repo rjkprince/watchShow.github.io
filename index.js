fcc657088209e373347bf47b9d557da4b7826cb3
rjkprince <60301066+rjkprince@users.noreply.github.com>
2020-06-06 21:39:35 +0530

    Update index.js

--- index.js
+++ index.js
@@ -14,6 +14,9 @@ setInterval(function () {
   var hour = new Date().getHours();
   var min = new Date().getMinutes();
   var sec = new Date().getSeconds();
+  if (hour < 10) hour = '0' + hour;
+  if (min < 10) min = '0' + min;
+  if (sec < 10) sec = '0' + sec;
   document.getElementById('hour').innerText = hour;
   document.getElementById('min').innerText = min;
   document.getElementById('sec').innerText = sec;
